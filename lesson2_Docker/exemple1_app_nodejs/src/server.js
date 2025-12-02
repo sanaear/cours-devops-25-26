4d226b3ab1d4faf146e87d54e503234ead58810d
sana achour <166882949+sanaear@users.noreply.github.com>
2025-12-02 10:28:15 +0100

    Add author name to welcome message

--- lesson2_Docker/exemple1_app_nodejs/src/server.js
+++ lesson2_Docker/exemple1_app_nodejs/src/server.js
@@ -4,6 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`
     <h1>Welcome to my awesome app!</h1>
+    <h2>sana achour</h2>
     <img src="https://i.pinimg.com/originals/cb/22/60/cb22603604e36d1caab7cc4e729506c9.gif" alt="Beautiful Landscape" style="max-width:400px;">
   `);
 });
